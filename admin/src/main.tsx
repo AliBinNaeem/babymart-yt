491e114c23a9c1f2a6a69d86915c318df6385a8b
AliBinNaeem <smabn5@gmail.com>
2025-11-04 16:58:12 +0500

    001

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router' ///https://youtu.be/sE_F02e76dQ?t=842
 import './index.css'
 import App from './App.tsx'
+import Login from './pages/Login.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}/> //https://youtu.be/sE_F02e76dQ?t=858
 )
